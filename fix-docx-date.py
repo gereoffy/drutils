358b8c1382182a16b7d54772fbc674910d544039
gereoffy <arpi.git@interoot.hu>
2023-10-15 11:24:32 +0000

    ugly tool to fix docx/xlsx/pptx file dates using info from xml

---
+++ fix-docx-date.py
@@ -0,0 +1,30 @@
+#! /usr/bin/python3.11
+
+import os,sys
+import zipfile
+import xml.etree.ElementTree as ET
+from datetime import datetime
+
+for fnev in sys.argv[1:] if len(sys.argv)>1 else ["f22341399.docx"]:
+  print(fnev)
+  datum=None
+  try:
+    with zipfile.ZipFile(fnev, mode="r") as zf:
+      data=zf.read("docProps/core.xml")
+      root = ET.fromstring(data)
+      for child in root:
+#        help(child)
+#        print(child.attrib)
+#        print(child.text)
+        try:
+          if child.text.startswith("20"):
+            d=str(child.text)
+#            print('"'+d+'"')
+            d=datetime.fromisoformat(d).timestamp()
+            if not datum or d>datum: datum=d
+#            print(d)
+#        print("\t".join(child.attrib.values()))
+        except: pass
+    if datum: os.utime(fnev, (datum,datum))
+  except Exception as e:
+    print(repr(e))
